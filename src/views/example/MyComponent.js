0b31b57e3de1d058b97a5eafba95c978e0bd01de
unknown <Vinh.pn260701@gmail.com>
2021-12-29 22:50:30 +0700

    4

--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -1,11 +1,13 @@
 import react from "react";
 import MyComponentChild from "./MyComponentChild";
 
+import AddComponent from "./AddComponent";
+
 class MyComponent extends react.Component {
 
     state = {
-        firstName: '',
-        lastName: '',
+        // firstName: '',
+        // lastName: '',
         arrJobs: [
             { id: 'abcJob1', title: 'Developers', salary: '500$' },
             { id: 'abcJob2', title: 'Testers', salary: '400$' },
@@ -13,24 +15,12 @@ class MyComponent extends react.Component {
         ]
     }
 
-    handleChaneFirstName = (event) => {
+    addNewJob = (job) => {
+        console.log('check job from parent: ', job)
         this.setState({
-            firstName: event.target.value
+            arrJobs: [...this.state.arrJobs, job]
         })
     }
-
-    handleChaneLastName = (event) => {
-        this.setState({
-            lastName: event.target.value
-        })
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault()
-        alert('click me')
-        console.log('>>> Check data input:', this.state)
-    }
-
     /* 
     JSX => return block (1 khoi div)
     <react.Fragment>  cach der tra ve nhieu hown 1 div 
@@ -40,34 +30,10 @@ class MyComponent extends react.Component {
 
         return (
             <>
-                <react.Fragment>
-                    <form >
-                        <label htmlFor="fname">First name:</label><br />
-                        <input
-                            type="text"
-                            value={this.state.firstName}
-                            onChange={(event) => this.handleChaneFirstName(event)}
-                        />
-                        <br />
-                        <label htmlFor="lname">Last name:</label><br />
-                        <input
-                            type="text"
-                            value={this.state.lastName}
-                            onChange={(event) => this.handleChaneLastName(event)}
-                        />
-                        <br /><br />
-                        <input
-                            type="submit"
-                            value="Submit"
-                            onClick={(event) => this.handleSubmit(event)}
-                        />
-                    </form>
-                </react.Fragment>
-
-
+                <AddComponent
+                    addNewJob={this.addNewJob}
+                />
                 <MyComponentChild
-                    name={this.state.firstName}
-                    age={'20'}
                     arrJobs={this.state.arrJobs}
                 />
             </>
